c3b26af601e41de49227f86633df00985e8d8a77
gaboolic <23441099@qq.com>
2026-01-16 16:48:20 +0800

    lua

--- lua/is_in_user_dict.lua
+++ lua/is_in_user_dict.lua
@@ -7,19 +7,23 @@ local M = {}
 function M.init(env)
     local config = env.engine.schema.config
     env.name_space = env.name_space:gsub('^*', '')
-    M.is_in_user_dict = config:get_bool(env.name_space)
+    M.is_in_user_dict = config:get_bool(env.name_space) or true
 end
 
 function M.func(input, env)
     for cand in input:iter() do
-        if M.is_in_user_dict then
-            if cand.type == "user_phrase" then
-                cand.comment = cand.comment .. '*'
-            end
-        else
-            if cand.type ~= "user_phrase" then
-                cand.comment = cand.comment .. '*'
-            end
+        -- 用户词库，加上*号
+        if cand.type == "user_phrase" then
+            cand.comment = cand.comment .. '*'
+        end
+        -- 用户置顶词
+        -- if cand.type == "user_table" then
+        --     cand.comment = cand.comment .. '⚡️'
+        -- end
+
+        -- 整句联想，加上𑄗符号
+        if cand.type == 'sentence' then
+            cand.comment = cand.comment .. '∞'
         end
         yield(cand)
     end
